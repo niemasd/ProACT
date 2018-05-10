d4b717047e36feb8b1c85935eee6b1b2fc5c2492
Niema Moshiri <niemamoshiri@gmail.com>
2018-05-10 14:55:27 -0700

    Added random selection

--- TreeBEARD.py
+++ TreeBEARD.py
@@ -10,6 +10,13 @@ def node_lt(self, other):
     return label(self) < label(other)
 Node.__lt__ = node_lt
 
+# randomly pick n individuals
+def random_select(tree,diag,n):
+    from random import shuffle
+    leaves = [label(leaf) for leaf in tree.leaf_node_iter()]
+    shuffle(leaves)
+    return leaves[:n]
+
 # sort all (or internal) nodes by average diagnosis time and output all leaves below current node (break ties by diagnosis time)
 def average(tree,diag,n,all):
     from queue import PriorityQueue,Queue; traverse = PriorityQueue()
@@ -53,29 +60,35 @@ def average_internal(tree,diag,n):
     return average(tree,diag,n,False)
 
 # run TreeBEARD
-METHODS = {'average_all':average_all,'average_internal':average_internal}
+METHODS = {'average_all':average_all,'average_internal':average_internal,'random':random_select}
+NEED_DIAGNOSIS = {'average_all','average_internal'}
 if __name__ == "__main__":
     import argparse; from gzip import open as gopen
     parser = argparse.ArgumentParser(description=__doc__, formatter_class=argparse.ArgumentDefaultsHelpFormatter)
     parser.add_argument('-t', '--tree', required=True, type=str, help="Input Tree File")
-    parser.add_argument('-d', '--diagnosis', required=True, type=str, help="Input Diagnosis Time File")
+    parser.add_argument('-d', '--diagnosis', required=False, type=str, default=None, help="Input Diagnosis Time File")
     parser.add_argument('-m', '--method', required=True, type=str, help="Method")
     parser.add_argument('-n', '--number', required=True, type=int, help="Number of Individuals")
     args = parser.parse_args()
     assert args.number > 0, "Number of individuals must be a positive integer"
+    args.method = args.method.lower()
     assert args.method in METHODS, "Invalid method: %s. Options: %s" % (args.method, ', '.join(sorted(METHODS.keys())))
+    if args.diagnosis is None:
+        assert args.method not in NEED_DIAGNOSIS, "Method %s requires diagnosis file" % args.method
+        diag = None
+    else:
+        diag = {}
+        for line in {True:gopen(args.diagnosis),False:open(args.diagnosis)}[args.diagnosis.lower().endswith('.gz')]:
+            if isinstance(line,bytes):
+                u,t = line.decode().strip().split()
+            else:
+                u,t = line.strip().split()
+            diag[u] = float(t)
     if args.tree.lower().endswith('.gz'):
         tree = Tree.get(data=gopen(args.tree).read().decode(),schema='newick')
     else:
         tree = Tree.get(data=open(args.tree).read(),schema='newick')
     num_leaves = len(tree.leaf_nodes())
     assert args.number < num_leaves, "Number of output individuals (%d) must be less than total number of individuals in tree (%d)" % (args.number,num_leaves)
-    diag = {}
-    for line in {True:gopen(args.diagnosis),False:open(args.diagnosis)}[args.diagnosis.lower().endswith('.gz')]:
-        if isinstance(line,bytes):
-            u,t = line.decode().strip().split()
-        else:
-            u,t = line.strip().split()
-        diag[u] = float(t)
     for u in METHODS[args.method](tree,diag,args.number):
         print(u)
