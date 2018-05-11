b3c0db870a29f1877f6224421260b4c267e266f4
Niema Moshiri <niemamoshiri@gmail.com>
2018-05-10 17:00:24 -0700

    Changed "diagnosis time" to "infection time"

--- TreeBEARD.py
+++ TreeBEARD.py
@@ -2,7 +2,7 @@
 from dendropy import Node,Tree
 from queue import PriorityQueue,Queue
 from warnings import warn
-UNUSED_DIAG_WARNING = "Specified diagnostic time file, but it will be ignored in this method"
+UNUSED_INF_WARNING = "Specified infection time file, but it will be ignored in this method"
 
 # get the label of Dendropy node u
 def label(u):
@@ -14,30 +14,30 @@ def node_lt(self, other):
 Node.__lt__ = node_lt
 
 # randomly pick n individuals
-def random_select(tree,diag,n):
-    if diag is not None:
-        warn(UNUSED_DIAG_WARNING)
+def random_select(tree,inf,n):
+    if inf is not None:
+        warn(UNUSED_INF_WARNING)
     from random import shuffle
     leaves = [label(leaf) for leaf in tree.leaf_node_iter()]
     shuffle(leaves)
     return leaves[:n]
 
-# sort all (or internal) nodes by average diagnosis time and output all leaves below current node (break ties by diagnosis time)
-def average(tree,diag,n,all):
+# sort all (or internal) nodes by average infection time and output all leaves below current node (break ties by infection time)
+def average(tree,inf,n,all):
     traverse = PriorityQueue()
     for u in tree.postorder_node_iter():
         u.done = False
         if u.is_leaf():
             u_label = label(u)
-            assert u_label in diag, "Individual %s not in diagnostic time file" % u_label
-            u.leaves_below = 1; u.tot_diag = diag[u_label]; u.avg_diag = diag[u_label]
+            assert u_label in inf, "Individual %s not in infection time file" % u_label
+            u.leaves_below = 1; u.tot_inf = inf[u_label]; u.avg_inf = inf[u_label]
         else:
-            u.leaves_below = 0.; u.tot_diag = 0.
+            u.leaves_below = 0.; u.tot_inf = 0.
             for c in u.child_node_iter():
-                u.leaves_below += c.leaves_below; u.tot_diag += c.tot_diag
-            u.avg_diag = u.tot_diag/u.leaves_below
+                u.leaves_below += c.leaves_below; u.tot_inf += c.tot_inf
+            u.avg_inf = u.tot_inf/u.leaves_below
         if all or not u.is_leaf():
-            traverse.put((-u.avg_diag,u))
+            traverse.put((-u.avg_inf,u))
     output = []
     while not traverse.empty():
         dummy,next = traverse.get(); next.done = True
@@ -50,7 +50,7 @@ def average(tree,diag,n,all):
         while not to_explore.empty():
             tmp = to_explore.get(); tmp.done = True
             if tmp.is_leaf():
-                pick.put((-diag[label(tmp)],tmp)); continue
+                pick.put((-inf[label(tmp)],tmp)); continue
             for c in tmp.child_node_iter():
                 if not c.done:
                     to_explore.put(c)
@@ -59,41 +59,41 @@ def average(tree,diag,n,all):
             if len(output) == n:
                 return output
     assert False, "Only found %d individuals, not specified number (%d)" % (len(output),n)
-def average_all(tree,diag,n):
-    return average(tree,diag,n,True)
-def average_internal(tree,diag,n):
-    return average(tree,diag,n,False)
+def average_all(tree,inf,n):
+    return average(tree,inf,n,True)
+def average_internal(tree,inf,n):
+    return average(tree,inf,n,False)
 
 # run TreeBEARD
 METHODS = {'average_all':average_all,'average_internal':average_internal,'random':random_select}
-NEED_DIAGNOSIS = {'average_all','average_internal'}
+NEED_INFECTION = {'average_all','average_internal'}
 if __name__ == "__main__":
     import argparse; from gzip import open as gopen
     parser = argparse.ArgumentParser(description=__doc__, formatter_class=argparse.ArgumentDefaultsHelpFormatter)
     parser.add_argument('-t', '--tree', required=True, type=str, help="Input Tree File")
-    parser.add_argument('-d', '--diagnosis', required=False, type=str, default=None, help="Input Diagnosis Time File")
-    parser.add_argument('-m', '--method', required=True, type=str, help="Method")
+    parser.add_argument('-i', '--infection', required=False, type=str, default=None, help="Input Infection Time File")
+    parser.add_argument('-m', '--method', required=True, type=str, help="Method (%s)" % ', '.join(sorted(METHODS.keys())))
     parser.add_argument('-n', '--number', required=True, type=int, help="Number of Individuals")
     args = parser.parse_args()
     assert args.number > 0, "Number of individuals must be a positive integer"
     args.method = args.method.lower()
     assert args.method in METHODS, "Invalid method: %s. Options: %s" % (args.method, ', '.join(sorted(METHODS.keys())))
-    if args.diagnosis is None:
-        assert args.method not in NEED_DIAGNOSIS, "Method %s requires diagnosis file" % args.method
-        diag = None
+    if args.infection is None:
+        assert args.method not in NEED_INFECTION, "Method %s requires infection file" % args.method
+        inf = None
     else:
-        diag = {}
-        for line in {True:gopen(args.diagnosis),False:open(args.diagnosis)}[args.diagnosis.lower().endswith('.gz')]:
+        inf = {}
+        for line in {True:gopen(args.infection),False:open(args.infection)}[args.infection.lower().endswith('.gz')]:
             if isinstance(line,bytes):
                 u,t = line.decode().strip().split()
             else:
                 u,t = line.strip().split()
-            diag[u] = float(t)
+            inf[u] = float(t)
     if args.tree.lower().endswith('.gz'):
         tree = Tree.get(data=gopen(args.tree).read().decode(),schema='newick')
     else:
         tree = Tree.get(data=open(args.tree).read(),schema='newick')
     num_leaves = len(tree.leaf_nodes())
     assert args.number < num_leaves, "Number of output individuals (%d) must be less than total number of individuals in tree (%d)" % (args.number,num_leaves)
-    for u in METHODS[args.method](tree,diag,args.number):
+    for u in METHODS[args.method](tree,inf,args.number):
         print(u)
