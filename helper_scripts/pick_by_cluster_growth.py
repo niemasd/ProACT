249f2ba6b1f2eee9e835d6b55b24816bb389ee6a
Niema Moshiri <niemamoshiri@gmail.com>
2019-02-19 14:27:43 -0800

    Fixed bug outputting more people than in clustering file

--- helper_scripts/pick_by_cluster_growth.py
+++ helper_scripts/pick_by_cluster_growth.py
@@ -10,7 +10,6 @@ if __name__ == "__main__":
     parser.add_argument('-c', '--clustering', required=True, type=str, help="Input Clustering File (Cluster Picker format)")
     parser.add_argument('-g', '--growth', required=True, type=str, help="Input Growth Rate File")
     parser.add_argument('-d', '--diagnosis', required=False, type=str, default=None, help="Diagnosis File")
-    parser.add_argument('-t', '--picktime', required=False, type=float, default=None, help="Pick Time")
     parser.add_argument('-n', '--number', required=False, type=str, default='All', help="Number of Individuals")
     parser.add_argument('-o', '--output', required=False, type=str, default='stdout', help="Output File")
     args = parser.parse_args()
@@ -24,9 +23,12 @@ if __name__ == "__main__":
         gf = open(args.growth)
     if args.diagnosis is not None:
         if args.diagnosis.lower().endswith('.gz'):
-            df = gopen(args.diagnosis)
+            df = gopen(args.diagnosis).read().decode().strip().splitlines()
         else:
-            df = open(args.diagnosis)
+            df = open(args.diagnosis).read().strip().splitlines()
+        everybody = {l.split('\t')[0].strip() for l in df}
+    else:
+        everybody = set()
     if args.output == 'stdout':
         from sys import stdout; output = stdout
     else:
@@ -39,17 +41,16 @@ if __name__ == "__main__":
             u,c = line.strip().split()
         if u == 'SequenceName':
             continue
-        cluster[u] = c; num_people += 1
+        cluster[u] = c; num_people += 1; everybody.add(u)
     if args.number == 'All':
-        args.number = num_people
+        args.number = len(everybody)
     else:
         args.number = int(args.number)
     assert args.number > 0, "Number of individuals must be a positive integer"
     if args.number > len(cluster):
         assert args.diagnosis is not None, "Number of output individuals (%d) is greater than the total number of individuals (%d), so must specify diagnosis file" % (args.number, len(cluster))
-        assert args.picktime is not None, "Number of output individuals (%d) is greater than the total number of individuals (%d), so must specify pick time" % (args.number, len(cluster))
-        warn("Number of output individuals (%d) is greater than the total number of individuals (%d), so the remaining %d individuals will be randomly selected from the diagnosis file (%s) before time %f." % (args.number, len(cluster), args.number-len(cluster), args.diagnosis, args.picktime))
-        everybody = {l.decode().split('\t')[0].strip() for l in df if float(l.decode().split('\t')[1]) < args.picktime}
+        warn("Number of output individuals (%d) is greater than the total number of individuals (%d), so the remaining %d individuals will be randomly selected from the diagnosis file (%s)." % (args.number, len(cluster), args.number-len(cluster), args.diagnosis))
+    from sys import stderr; stderr.write(str(len(everybody))); stderr.write('\n')
     growth = {}
     for line in gf:
         if isinstance(line,bytes):
@@ -59,9 +60,8 @@ if __name__ == "__main__":
         if c.startswith('Cluster') and g.startswith('GrowthRate'):
             continue
         growth[c] = float(g)
+    num_output = 0
     for g,u in sorted([(growth[cluster[u]],u) for u in cluster if cluster[u] in growth], reverse=True)[:args.number]:
-        output.write('%s\n'%u)
-        if args.number > len(cluster):
-            everybody.remove(u)
-    for _ in range(args.number-len(cluster)):
-        output.write('%s\n'%everybody.pop())
+        output.write('%s\n'%u); everybody.remove(u); num_output += 1
+    while num_output < args.number:
+        output.write('%s\n'%everybody.pop()); num_output += 1
