7b923043839b1f64e209f8630d43246092b87091
Niema Moshiri <niemamoshiri@gmail.com>
2019-03-03 18:04:11 -0800

    Remove script

--- helper_scripts/find_linear_combination_coefficients.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-'''
-Find the coefficients of the linear combination optimizing selection
-'''
-from sys import argv
-from treeswift import read_tree_newick
-if len(argv) != 3:
-    print("
