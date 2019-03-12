be5164b81ecb048caf98faeb37e89027fd6ef876
Niema Moshiri <niemamoshiri@gmail.com>
2019-03-11 19:36:22 -0700

    Changed back to abs(my root_to_tip minus avg root_to_tip)

--- ProACT.py
+++ ProACT.py
@@ -5,23 +5,24 @@ from warnings import warn
 # sort by edge length, then parent edge length, then grandparent edge length, etc.
 def prioritize(tree,n):
     # compute path-length to root
-    leaves = list(); root_dist = dict(); root_dist_u = dict(); score = dict()
+    leaves = list(); root_dist = dict()#; root_dist_u = dict()
     for u in tree.traverse_preorder():
         if u.is_root():
-            root_dist[u] = 0; root_dist_u[u] = 0
+            root_dist[u] = 0#; root_dist_u[u] = 0
         else:
             if u.edge_length is None:
                 u.edge_length = 0
-            root_dist[u] = root_dist[u.parent] + u.edge_length; root_dist_u[u] = root_dist_u[u.parent] + 1
+            root_dist[u] = root_dist[u.parent] + u.edge_length#; root_dist_u[u] = root_dist_u[u.parent] + 1
         if u.is_leaf():
-            leaves.append(u); score[u] = (u.edge_length, root_dist[u]/root_dist_u[u])
+            leaves.append(u)
+    avg_root_dist = sum(root_dist[u] for u in leaves)/len(leaves)
     if n == 'All':
         n = len(leaves)
     else:
         n = int(n)
     if n < 0 or n > len(leaves):
         raise ValueError("Number of output individuals (%d) must be less than or equal to total number of individuals in tree (%d)" % (n,len(leaves)))
-    return [l.label for l in sorted(leaves, key=lambda x: score[x])[:n]]
+    return [l.label for l in sorted(leaves, key=lambda x: (x.edge_length, abs(root_dist[x]-avg_root_dist)))[:n]]
 
 # run ProACT
 if __name__ == "__main__":
