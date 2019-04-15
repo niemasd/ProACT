2f104c3b6eb49f848188cfc1f7959b66d073bfe4
Niema Moshiri <niemamoshiri@gmail.com>
2019-04-15 14:42:45 -0700

    Changed tie-breaker to average edge lengths to root

--- ProACT.py
+++ ProACT.py
@@ -5,16 +5,17 @@ from warnings import warn
 # sort by edge length, then parent edge length, then grandparent edge length, etc.
 def prioritize(tree,n):
     # compute path-length to root
-    leaves = list(); root_dist = dict()
+    leaves = list(); root_dist = dict(); num_ancestors = dict()
     for u in tree.traverse_preorder():
         if u.is_leaf():
             leaves.append(u)
         if u.is_root():
-            root_dist[u] = 0
+            root_dist[u] = 0; num_ancestors[u] = 0; u.edge_length = 0
         else:
             if u.edge_length is None:
                 u.edge_length = 0
             root_dist[u] = root_dist[u.parent] + u.edge_length
+            num_ancestors[u] = num_ancestors[u.parent] + 1
     if n == 'All':
         n = len(leaves)
     else:
@@ -23,7 +24,7 @@ def prioritize(tree,n):
         raise ValueError("Number of output individuals (%d) must be less than or equal to total number of individuals in tree (%d)" % (n,len(leaves)))
     # sort by edge length, then parent edge length, then grandparent edge length, etc. (use dist to root once ancestors run out)
     def compare(l1,l2):
-        l1r = root_dist[l1]; l2r = root_dist[l2]
+        tie1 = float(root_dist[l1])/num_ancestors[l1]; tie2 = float(root_dist[l2])/num_ancestors[l2]
         c1 = l1; c2 = l2
         c1l = c1.edge_length; c2l = c2.edge_length
         while c1l == c2l:
@@ -31,12 +32,12 @@ def prioritize(tree,n):
                 return False
             if c1 is not None:
                 if c1.parent is None:
-                    c1l = l1r; c1 = None
+                    c1l = tie1; c1 = None
                 else:
                     c1 = c1.parent; c1l = c1.edge_length
             if c2 is not None:
                 if c2.parent is None:
-                    c2l = l2r; c2 = None
+                    c2l = tie2; c2 = None
                 else:
                     c2 = c2.parent; c2l = c2.edge_length
         return c1l < c2l
